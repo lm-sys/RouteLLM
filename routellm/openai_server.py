b954d65b2221f83e847d406217f43b4ede605cf2
Isaac Ong <isaacong.jw@gmail.com>
2024-07-06 21:57:40 -0700

    Update server to use controller

--- routellm/openai_server.py
+++ routellm/openai_server.py
@@ -4,7 +4,6 @@ It current only supports Chat Completions: https://platform.openai.com/docs/api-
 """
 
 import argparse
-import asyncio
 import logging
 import os
 import time
@@ -13,20 +12,19 @@ from typing import AsyncGenerator, Dict, List, Literal, Optional, Union
 
 import fastapi
 import shortuuid
-import tqdm
 import uvicorn
 import yaml
 from fastapi.concurrency import asynccontextmanager
 from fastapi.responses import JSONResponse, StreamingResponse
-from litellm import acompletion
 from openai import AsyncOpenAI
 from pydantic import BaseModel, Field
 
+from routellm.controller import Controller, RoutingError
 from routellm.model_pair import ModelPair
 from routellm.routers.routers import ROUTER_CLS
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-ROUTERS_MAP = {}
+CONTROLLER = None
 
 openai_client = AsyncOpenAI()
 count = defaultdict(lambda: defaultdict(int))
@@ -34,13 +32,19 @@ count = defaultdict(lambda: defaultdict(int))
 
 @asynccontextmanager
 async def lifespan(app):
-    router_pbar = tqdm.tqdm(args.routers)
-    for router in router_pbar:
-        router_pbar.set_description(f"Loading {router}")
-        router_config = config.get(router, {})
-        ROUTERS_MAP[router] = ROUTER_CLS[router](**router_config)
+    global CONTROLLER
+
+    routed_pair = ModelPair(strong=args.strong_model, weak=args.weak_model)
+    CONTROLLER = Controller(
+        args.routers,
+        yaml.safe_load(open(args.config, "r")),
+        routed_pair,
+        args.alt_base_url,
+        args.alt_api_key,
+        progress_bar=True,
+    )
     yield
-    ROUTERS_MAP.clear()
+    CONTROLLER = None
 
 
 app = fastapi.FastAPI(lifespan=lifespan)
@@ -116,57 +120,25 @@ async def create_chat_completion(request: ChatCompletionRequest):
     # The model name field contains the parameters for routing.
     # Model name uses format router-[router name]-[threshold] e.g. router-bert-0.7
     # The router type and threshold is used for routing that specific request.
-    _, router, threshold = request.model.split("-", 2)
-    logging.info(f"Received request for router {router} and threshold {threshold}")
-    if not request.model.startswith("router"):
-        return JSONResponse(
-            ErrorResponse(
-                message=f"Invalid model {request.model}. Model name must be of the format 'router-[router name]-[threshold]'."
-            ).model_dump(),
-            status_code=400,
+    logging.info(f"Received request: {request}")
+    try:
+        res = await CONTROLLER.acompletion(
+            **request.model_dump(exclude_none=True),
         )
-    elif router not in ROUTERS_MAP:
+    except RoutingError as e:
         return JSONResponse(
-            ErrorResponse(
-                message=f"Invalid router {router}. Available routers are {list(ROUTERS_MAP.keys())}."
-            ).model_dump(),
-            status_code=400,
-        )
-    elif not 0.0 <= float(threshold) <= 1.0:
-        return JSONResponse(
-            ErrorResponse(
-                message=f"Invalid threshold {threshold}. Threshold must be a float between 0.0 and 1.0."
-            ).model_dump(),
+            ErrorResponse(message=str(e)).model_dump(),
             status_code=400,
         )
 
-    threshold = float(threshold)
-
-    # Look at the last turn for routing.
-    # Our current routers were only trained on first turn data, so more research is required here.
-    prompt = request.messages[-1]["content"]
-
-    route_fn = ROUTERS_MAP[router].route
-    if asyncio.iscoroutinefunction(route_fn):
-        routed_model = await route_fn(prompt, threshold, ROUTED_PAIR)
-    else:
-        routed_model = route_fn(prompt, threshold, ROUTED_PAIR)
-    count[router][routed_model] += 1
-    logging.info(f"Model Counts: {dict(count)}")
-
-    generator = await acompletion(
-        model=routed_model,
-        api_base=API_BASE,
-        api_key=API_KEY,
-        **request.model_dump(exclude=["model"], exclude_none=True),
-    )
+    logging.info(CONTROLLER.model_counts)
 
     if request.stream:
         return StreamingResponse(
-            content=stream_response(generator), media_type="text/event-stream"
+            content=stream_response(res), media_type="text/event-stream"
         )
     else:
-        return JSONResponse(content=generator.model_dump())
+        return JSONResponse(content=res.model_dump())
 
 
 parser = argparse.ArgumentParser(
@@ -203,17 +175,6 @@ parser.add_argument(
     "--weak-model", type=str, default="anyscale/mistralai/Mixtral-8x7B-Instruct-v0.1"
 )
 args = parser.parse_args()
-
-config = yaml.safe_load(open(args.config, "r"))
-
-alt_client = AsyncOpenAI(
-    base_url=args.alt_base_url,
-    api_key=args.alt_api_key,
-)
-
-ROUTED_PAIR = ModelPair(strong=args.strong_model, weak=args.weak_model)
-API_BASE = args.alt_base_url
-API_KEY = args.alt_api_key
 
 if args.verbose:
     logging.basicConfig(level=logging.INFO)
